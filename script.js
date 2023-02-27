0dd0f2900d432eb9242de13d5c42e0f282df8360
sd <sd_net@mail.ru>
2023-02-27 21:45:16 +0300

    rel01

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const buttons = document.querySelector('.calc__buttons');
 const radioBox = document.querySelector('.radio-box');
 
 const arrNumBtn = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const arrActionBtn = ['equals', 'plus', 'minus', 'div', 'multipl'];
+const arrActionBtn = ['=', '+', '-', '/', '*'];
 
 let result = 0;
-let check = false;
+let numberA = '';
+let numberB = '';
 let actionMem = '';
-let number1 = '0';
-let number2 = '0';
 let expression = '';
 
 
@@ -33,18 +32,18 @@ function radioClick(){
 
 function handlerBtn(event){
     if(event.target.nodeName === 'BUTTON'){
-        const btn = event.target.dataset.btn;
+        const btnKey = event.target.dataset.btn;
         //console.log(event.target.dataset.btn)
-        if(arrNumBtn.includes(btn)){
-            numBtn(btn);
+        if(arrNumBtn.includes(btnKey)){
+            numBtn(btnKey);
         }
-        if(arrActionBtn.includes(btn)){
-            actionBtn(btn);
+        if(arrActionBtn.includes(btnKey)){
+            actionBtn(btnKey);
         }
-        if(btn === 'reset'){
+        if(btnKey === 'reset'){
             resetBtn();
         }
-        if(btn === 'del'){
+        if(btnKey === 'del'){
             delBtn();
         }
     }
@@ -57,93 +56,90 @@ function setOutput(out){
     output.innerText = out;
 }
 
-//обработка чиловых кнопок + точка
-function numBtn(num){ 
-    number2 += num;
-    //отрезаем лишний ноль в начале строки
-     if(number2[0] == '0'){
-       number2 = number2.slice(1);
-    }  
-    setOutput(number2);
+function setNumber(num,btnKey){
+    num += btnKey;
+    /* if(num[0] === '0')
+        num = num.slice(1); */
+    
+    setOutput(+num);
+    return +num;
+}
+
+function numBtn(btnKey){ 
+    if(numberB === '' &&  actionMem === ''){
+        numberA = setNumber(numberA, btnKey);
+    }else{
+        numberB = setNumber(numberB, btnKey);        
+    } 
+    console.log(numberA, ' - ', numberB)
 }
 
 function delBtn(){
-    if(number2.length > 0){
-        number2 = number2.slice(0, -1);
-        if(number2.length === 0)
-            number2 = 0;
+    if(numberB === ''){
+        numberA = deleteNum(numberA);
+    }else{
+        numberB = deleteNum(numberB);
     }
-    setOutput(number2);
+}
 
-    //console.log(`number1 = ${number1} | number2 = ${number2} | result = ${result} | actionMem = ${actionMem} expression = ${expression}`);
+function deleteNum(num){
+    num = String(num);
+    num = num.slice(0,-1);
+    if(num.length === 0){
+        num = 0;
+    }
+    setOutput(num);
+    return num;
 }
 
 function resetBtn(){
     result = 0;
-    number1 = 0;
-    number2 = 0;
+    numberA = '';
+    numberB = '';
     actionMem = '';
-    expression = ''
-    setOutput(result);
+    expression = '';
+    setOutput('');
+    console.log('reset')
 }
 
 function actionBtn(action){
-    if(number1 === '0'){
-        number1 = number2;
-        number2 = '0';
+    if(action && numberB === ''){
         actionMem = action;
-
-        expression = number1 + actionMem + number2;
-        console.log(`number1 = ${number1} | number2 = ${number2} | result = ${result} | actionMem = ${actionMem} expression = ${expression}`);
+    }else if(action === '='){
+        actionBtn(actionMem);
     }else{
+        expression = numberA + ' ' + actionMem + ' ' + numberB;
+        actionSwith(actionMem);
+        numberB = '';
+        actionMem = action;
+        setOutput(numberA);        
+        console.log(`numberA = ${numberA} | numberB = ${numberB} | result = ${numberA} | actionMem = ${actionMem} expression = ${expression}`);
+    }     
 
-    
-        switch(actionMem){
-            case 'plus':
-                number1 = Number(number1) + Number(number2);
-                nulls('plus')
-                console.log(`number1 = ${number1} | number2 = ${number2} | result = ${result} | actionMem = ${actionMem} expression = ${expression}`);
-                break;
-            case 'minus':
-                result = Number(number1) - Number(number2);
-                break;
-            case 'div':
-                result = Number(number1) / Number(number2);
-                break;
-            case 'multipl':
-                result = Number(number1) * Number(number2);
-                break;
-            default:
-                console.log('equals');
-                resetBtn(); 
-                               
-                break;
-        }
-        setOutput(number1);
-    }       
-    
 }
 
-function nulls(action){
-    //number1 = res;
-    number2 = '0';
-    actionMem = action;
+function actionSwith(action){
+    switch(action){
+        case '+':
+            numberA = +numberA + +numberB;  
+            break;
+        case '-':
+            numberA = +numberA - +numberB;
+            break;
+        case '*':
+            numberA = +numberA * +numberB;
+            break;
+        case '/':   
+            numberA = +numberA / +numberB;
+            break;
+    }
 }
-
-console.log(`number1 = ${number1} | number2 = ${number2} | result = ${result} | actionMem = ${actionMem} expression = ${expression}`);
-
-
-
-
-
-
-
-
-
-
 
 function handlerKeys(event){
     console.log(event)
     if(arrNumBtn.includes(event.key))
         numBtn(event.key);
+    if(event.key === '+'){
+        actionBtn('+',false);
+    }
 }
